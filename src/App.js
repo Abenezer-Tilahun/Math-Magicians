527896d3f36d914060d07851d728178e198376b7
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-03-22 15:52:44 +0300

    Design a Calculator Component and Remove boilerplate from CRA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;

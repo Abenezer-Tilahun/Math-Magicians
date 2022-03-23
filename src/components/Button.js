da9bea63f4e42e951f17f898f2ae580da00ec04f
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-03-23 11:49:46 +0300

    Covert Buttons to React componenta And Connect UI to Logic

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { value, onClick } = this.props;
+    let className = /[0-9.AC/%/]/.test(value) ? 'btn' : 'btn btn-operation';
+    className += value === 'AC/%/+/-' ? 'btn' : '';
+    className += value === '0' ? ' btn-0' : '';
+
+    return (
+      <button
+        className={className}
+        onClick={() => onClick(value)}
+        type="button"
+      >
+        {value}
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  onClick: () => null,
+};

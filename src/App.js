0657896aca7f1c58ccfb9ecc01db884bf6fdfdbd
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-03-30 16:14:51 +0300

    Add Home, Quote page and Basic routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main id="tabs">
-        <Calculator />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
